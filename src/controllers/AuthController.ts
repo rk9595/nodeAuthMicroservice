1fe61357686e9ea9e596b0406ce7dcdd304e738b
rk9595 <rakesh.kariya@somaiya.edu>
2023-11-19 12:01:43 +0530

    test-added-register-user

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,13 @@
+import { Response } from "express";
+ import { RegisterUserRequest } from "../types";
+ import { UserService } from "../services/UserService";
+
+ export class AuthController {
+     constructor(private userService: UserService) {}
+
+     async register(req: RegisterUserRequest, res: Response) {
+         const { firstName, lastName, email, password } = req.body;
+         await this.userService.create({ firstName, lastName, email, password });
+         res.status(201).json();
+     }
+ }
---
+++ tests/users/register.spec.ts
@@ -0,0 +1,82 @@
+import request from "supertest";
+import app from "../../src/app";
+import { User } from "../../src/entity/User";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../src/config/data-source";
+import { truncateTables } from "../utils";
+
+describe("POST /auth/register", () => {
+    let connection: DataSource;
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    beforeEach(async () => {
+        // Database truncate
+        await truncateTables(connection);
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
+    describe("Given all fields", () => {
+        it("should return the 201 status code", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "secret",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            // Assert
+            expect(response.statusCode).toBe(201);
+        });
+
+        it("should return valid json response", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "secret",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            // Assert application/json utf-8
+            expect(
+                (response.headers as Record<string, string>)["content-type"],
+            ).toEqual(expect.stringContaining("json"));
+        });
+
+        it("should persist the user in the database", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "secret",
+            };
+            // Act
+            await request(app).post("/auth/register").send(userData);
+
+            // Assert
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            expect(users).toHaveLength(1);
+            expect(users[0].firstName).toBe(userData.firstName);
+            expect(users[0].lastName).toBe(userData.lastName);
+            expect(users[0].email).toBe(userData.email);
+        });
+    });
+    describe("Fields are missing", () => {});
+});
