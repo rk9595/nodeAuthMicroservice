a4dee7a1fb527d55cf7bf156c07fbd94cd5bea59
rk9595 <rakesh.kariya@somaiya.edu>
2023-11-26 18:42:02 +0530

    brcypt-for-password

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,11 +3,23 @@ import { User } from "../entity/User";
 import { UserData } from "../types";
 import createHttpError from "http-errors";
 import { Roles } from "../constants";
+import bcrypt from "bcrypt";
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
     async create({ firstName, lastName, email, password }: UserData) {
+        const user = await this.userRepository.findOne({
+            where: { email: email },
+        });
+        if (user) {
+            const err = createHttpError(400, "Email is already exists!");
+            throw err;
+        }
+        // Hash the password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
         try {
             return await this.userRepository.save({
                 firstName,
