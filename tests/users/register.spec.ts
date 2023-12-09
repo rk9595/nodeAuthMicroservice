3f7da43f482f69055393361484980db20b09552e
rk9595 <rakesh.kariya@somaiya.edu>
2023-12-09 11:04:00 +0530

    test-token

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -4,6 +4,7 @@ import { User } from "../../src/entity/User";
 import { DataSource } from "typeorm";
 import { AppDataSource } from "../../src/config/data-source";
 import { Roles } from "../../src/constants";
+import { isJwt } from "../utils";
 
 describe("POST /auth/register", () => {
   let connection: DataSource;
@@ -28,7 +29,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "password",
       };
       // Act
@@ -43,7 +44,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "password",
       };
       // Act
@@ -60,7 +61,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "password",
       };
       // Act
@@ -80,7 +81,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "password",
       };
       // Act
@@ -98,7 +99,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "password",
       };
       // Act
@@ -116,7 +117,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "password",
       };
       // Act
@@ -135,7 +136,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "password",
       };
       const userRepository = connection.getRepository(User);
@@ -148,6 +149,42 @@ describe("POST /auth/register", () => {
       // Assert
       expect(response.statusCode).toBe(400);
       expect(users).toHaveLength(1);
+    });
+
+    it("should return the access token and refresh token inside a cookie", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Rakesh",
+        lastName: "K",
+        email: "rakesh@nodeAuth.com",
+        password: "password",
+      };
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      interface Headers {
+        ["set-cookie"]: string[];
+      }
+      // Assert
+      let accessToken = null;
+      let refreshToken = null;
+      const cookies = (response.headers as Headers)["set-cookie"] || [];
+      // accessToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjkzOTA5Mjc2LCJleHAiOjE2OTM5MDkzMzYsImlzcyI6Im1lcm5zcGFjZSJ9.KetQMEzY36vxhO6WKwSR-P_feRU1yI-nJtp6RhCEZQTPlQlmVsNTP7mO-qfCdBr0gszxHi9Jd1mqf-hGhfiK8BRA_Zy2CH9xpPTBud_luqLMvfPiz3gYR24jPjDxfZJscdhE_AIL6Uv2fxCKvLba17X0WbefJSy4rtx3ZyLkbnnbelIqu5J5_7lz4aIkHjt-rb_sBaoQ0l8wE5KzyDNy7mGUf7cI_yR8D8VlO7x9llbhvCHF8ts6YSBRBt_e2Mjg5txtfBaDq5auCTXQ2lmnJtMb75t1nAFu8KwQPrDYmwtGZDkHUcpQhlP7R-y3H99YnrWpXbP8Zr_oO67hWnoCSw; Max-Age=43200; Domain=localhost; Path=/; Expires=Tue, 05 Sep 2023 22:21:16 GMT; HttpOnly; SameSite=Strict
+      cookies.forEach((cookie) => {
+        if (cookie.startsWith("accessToken=")) {
+          accessToken = cookie.split(";")[0].split("=")[1];
+        }
+
+        if (cookie.startsWith("refreshToken=")) {
+          refreshToken = cookie.split(";")[0].split("=")[1];
+        }
+      });
+      expect(accessToken).not.toBeNull();
+      expect(refreshToken).not.toBeNull();
+
+      expect(isJwt(accessToken)).toBeTruthy();
+      expect(isJwt(refreshToken)).toBeTruthy();
     });
   });
   describe("Fields are missing", () => {
@@ -174,7 +211,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "password",
       };
       // Act
@@ -191,7 +228,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "password",
       };
       // Act
@@ -209,7 +246,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "",
       };
       // Act
@@ -229,7 +266,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: " rakesh@mern.space ",
+        email: " rakesh@nodeAuth.com ",
         password: "password",
       };
       // Act
@@ -239,7 +276,7 @@ describe("POST /auth/register", () => {
       const userRepository = connection.getRepository(User);
       const users = await userRepository.find();
       const user = users[0];
-      expect(user.email).toBe("rakesh@mern.space");
+      expect(user.email).toBe("rakesh@nodeAuth.com");
     });
     it("should return 400 status code if email is not a valid email", async () => {
       // Arrange
@@ -263,7 +300,7 @@ describe("POST /auth/register", () => {
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
-        email: "rakesh@mern.space",
+        email: "rakesh@nodeAuth.com",
         password: "pass", // less than 8 chars
       };
       // Act
@@ -275,14 +312,13 @@ describe("POST /auth/register", () => {
       const users = await userRepository.find();
       expect(users).toHaveLength(0);
     });
-
     it("shoud return an array of error messages if email is missing", async () => {
       // Arrange
       const userData = {
         firstName: "Rakesh",
         lastName: "K",
         email: "",
-        password: "password", // less than 8 chars
+        password: "password",
       };
       // Act
       const response = await request(app).post("/auth/register").send(userData);
